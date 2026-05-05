9db05042b3091ed6bedd95f0be83234320e423bf
Khoi <khoanote999999@gmail.com>
2026-05-05 14:24:10 +0700

    feat: initialize Django project settings and configure static, media, and authentication paths

--- 12_501240178_LeDoDangKhoi/social/social/socialproject/socialproject/settings.py
+++ 12_501240178_LeDoDangKhoi/social/social/socialproject/socialproject/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# THÊM DÒNG NÀY:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
